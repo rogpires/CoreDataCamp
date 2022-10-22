788940d37d4a8cc455d2b71e6fa48c240eebb5ba
rogpires <rogeriocpires@hotmail.com>
2022-10-22 19:18:42 -0300

    Initial Commit

---
+++ CoreDataCamp/CoreDataCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataCampApp.swift
+//  CoreDataCamp
+//
+//  Created by Rogerio Pires on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
